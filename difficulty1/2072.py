fd9e2c3ec9d7660b65b8ca9deef805300f75d944
gandolkim <gandolkim@gmail.com>
2020-01-28 19:43:41 +0900

    2020.01.28 commit

---
+++ difficulty1/2072.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+num_list = []
+result_list = []
+count_num = int(input())
+
+for _ in range(count_num):
+    num_list.append(input())
+
+for i in range(count_num):
+    num_str = []
+    sum = 0
+    num_str = num_list[i].split(' ')
+ 
+    for j in num_str:
+        if int(j) % 2 == 1:
+            sum += int(j) 
+          
+    result_list.append(sum)
+
+for i in range(count_num):
+    print(f'#{i+1} {result_list[i]} ')
