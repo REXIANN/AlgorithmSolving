78b61a0d4afe546a864b8b297a4557326c5e7d63
김진혁 <gandolkim@gmail.com>
2020-11-20 16:48:40 +0900

    test

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
-a = 1
-b = 2
-c = 3 
-d = 4
-print(a, b)
+from time import time
+
+num_list = [i for i in range(1, 10 ** 8)]
+num_set = set(i for i in range(1, 10 ** 8))
+
+print("Start analysis")
+number = 10 ** 8 - 3
+start_time = time()
+if number in num_list:
+    print("number is in num_list")
+    print(time() - start_time)
+
+start_time = time()
+if number in num_set:
+    print("number is in num_set")
+    print(time() - start_time)
