c43f16fe200899750e2ad249fdacbdb277d872dc
김진혁 <gandolkim@gmail.com>
2020-11-24 17:35:02 +0900

    가장 큰 수 - 진행중

--- programmers/가장큰수.py
+++ programmers/가장큰수.py
@@ -1,7 +1,19 @@
 def solution(numbers):
-    numbers = sorted(list(map(str, numbers)), reverse=True)
-    return numbers
-
+    numbers = sorted(map(str, numbers), reverse=True)
+    orders = [[] for _ in range(10)]
+    for number in numbers:
+        orders[int(number[0])].append(number)
+    
+    
+    for i in range(len(orders)):
+        orders[i].sort(key = lambda x: x[0] if len(x) == 1 else x[0] if x[1] < x[0] else x[1], reverse = True)
+    
+    # print(orders)
+    result = []
+    for i in range(9, -1, -1):
+        result.append(''.join(orders[i]))
+        
+    return ''.join(result)
 
 numbers = [3, 30, 34, 5, 9]
 print(solution(numbers))
