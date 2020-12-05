a3390e8b4798ba1c1166ae75c03400aafeb2dbf3
김진혁 <gandolkim@gmail.com>
2020-12-05 23:11:30 +0900

    청소년 상어

--- baekjoon/19236.py
+++ baekjoon/19236.py
@@ -6,5 +6,22 @@ sys.stdin = open('19236input.txt', 'r')
 dr = [-1, -1, 0, 1, 1, 1, 0, -1]
 dc = [0, -1, -1, -1, 0, 1, 1, 1]
 
-fishes = [0] * 16
-fish_dir = [0] * 16
+def find_fishes(matrix):
+    result = [0] * 16
+    for i in range(4):
+        for j in range(4):
+            f, d = matrix[i][j][0], matrix[i][j][1] 
+            result[f] = d
+    return result
+
+
+matrix = []
+for i in range(4):
+    orders = list(map(int, input().split()))
+    # 실제 숫자가 1 ~ 16 이므로 1씩 빼줘야 함
+    arr = [(orders[j] - 1, orders[j + 1]) for j in range(0, 8, 2)]
+    matrix.append(arr)
+print(matrix)
+
+fish_dir = find_fishes(matrix)
+print(fish_dir)
