06c8c9a1bf0ea8aceb11acfcb02b6cefd269b170
김진혁 <gandolkim@gmail.com>
2020-12-12 09:50:26 +0900

    updated

---
+++ programmers/수식최대화.py
@@ -0,0 +1,17 @@
+def solution(expression):
+    answer = []
+    head, tail = 0, 0
+    while head < len(expression):
+        if expression[head] in "+-*":
+            answer.append(int(expression[tail: head]))
+            answer.append(expression[head])
+            head, tail = head + 1, head + 1
+        else:
+            head += 1
+    answer.append(int(expression[tail:head]))
+    
+    return answer
+
+
+expression = "100-200*300-500+20"
+print(solution(expression))
