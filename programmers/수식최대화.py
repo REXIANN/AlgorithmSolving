e493ca2b97716a005a2d52038a43eaa6480fa538
김진혁 <gandolkim@gmail.com>
2020-12-12 21:16:34 +0900

    수식최대화

--- programmers/수식최대화.py
+++ programmers/수식최대화.py
@@ -1,7 +1,20 @@
 from itertools import permutations
 
-def calc(experssion, operand):
+# define calc function
+def calc(array, operand):
     stack = []
+    array = array[::-1]
+    #print('calc', array, operand)
+    while array:
+        elem = array.pop()
+        if elem == operand:
+            first_num = stack.pop()
+            second_num = array.pop()
+            result = eval(str(first_num) + operand + str(second_num))
+            stack.append(result)
+        else:
+            stack.append(elem)
+    return stack[::-1]
 
 
 def solution(expression):
@@ -14,12 +27,18 @@ def solution(expression):
             head, tail = head + 1, head + 1
         else:
             head += 1
-    
     answer.append(int(expression[tail:head]))
+    #print('sol', answer)
+
+    max_value = 0
     for permutation in permutations('+-*'):
-        print(permutation)
-    
-    return answer
+        array = answer[:]
+        for operand in permutation:
+            array = calc(array, operand)
+
+        value = abs(array[0])    
+        max_value = value if value > max_value else max_value
+    return max_value
 
 
 expression = "100-200*300-500+20"
