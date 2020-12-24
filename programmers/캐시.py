36d77c8524cb8d1ba4cbda7ab3448a7b2631bc45
김진혁 <gandolkim@gmail.com>
2020-12-24 21:36:11 +0900

    캐시

--- programmers/캐시.py
+++ programmers/캐시.py
@@ -1,32 +1,38 @@
 from collections import deque
-import heapq
 
 def solution(cacheSize, cities):
-    cache = []
-    cache_set = set()
+    h = [[0, 'a'] for _ in range(cacheSize)]
+    h_set = set()
     count = 0
-    if not cacheSize:
-        return 5 * len(cities)
-    
+    # heapq를 직접 구현하는게 핵심 
+
     for city in cities:
         city = city.lower()
-        if city in cache_set:
-            count += 5
-            for i in range(len(cache)):
-                if cache[i][1] == city:
-                    cache[i][0] += 1           
-        else:
+        if city in h_set:
+            for hh in h:
+                if hh[1] == city:
+                    hh[0] += 1
+                    break
             count += 1
-            if len(cache_set) == cacheSize:
-                del_city = heapq.heappop(cache)
-                cache_set.remove(del_city[1])
-            heapq.heappush(cache, [1, city])
-            cache_set.add(city)
-            
+        else:
+            if h:
+                rm = heapq.heappop(h)
+                if rm[1] in h_set:
+                    h_set.remove(rm[1])
+            heapq.heappush(h, [1, city])
+            h_set.add(city)
+            count += 5
+        print(h, count)
+    
     return count
 
+cacheSize = 2
 
-cacheSize = 3	
-cities = ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]	
+cities1 = ['Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA', 'Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA']
+cities2 = ['Jeju', 'Pangyo', 'Seoul', 'Jeju', 'Pangyo', 'Seoul', 'Jeju', 'Pangyo', 'Seoul']
+cities3 = ['Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA', 'SanFrancisco', 'Seoul', 'Rome', 'Paris', 'Jeju', 'NewYork', 'Rome']
+cities4 = ['Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA', 'SanFrancisco', 'Seoul', 'Rome', 'Paris', 'Jeju', 'NewYork', 'Rome']
+cities5 = ['Jeju', 'Pangyo', 'NewYork', 'newyork']
+cities6 = ['Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA']
 
-print(solution(cacheSize, cities))
+print(solution(cacheSize, cities3))
