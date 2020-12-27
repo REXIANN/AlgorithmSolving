18e0612787856ac5f35b41358af9786a6939b267
김진혁 <gandolkim@gmail.com>
2020-12-27 15:55:50 +0900

    여행경로 continue

--- programmers/여행경로.py
+++ programmers/여행경로.py
@@ -2,8 +2,15 @@ def solution(tickets):
     answer = []
     airports = dict()
     for ticket in tickets:
-        if airports.get(ticket):
-            airports[ticket].append(ticket)
+        if airports.get(ticket[0]):
+            airports[ticket[0]].append(ticket[1])
         else:
-            airports[ticket] = [ticket]
+            airports[ticket[0]] = [ticket[1]]
+
+    print(airports)
+
     return answer
+
+
+tickets = [['ICN', 'SFO'], ['ICN', 'ATL'], ['SFO', 'ATL'], ['ATL', 'ICN'], ['ATL','SFO']]
+print(solution(tickets))
