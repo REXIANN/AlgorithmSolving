e66d2b7bc89969ad854646f5b84c2e39d41f80b2
김진혁 <gandolkim@gmail.com>
2020-12-29 18:13:19 +0900

    여행경로

--- programmers/여행경로.py
+++ programmers/여행경로.py
@@ -1,42 +1,66 @@
-from collections import deque
+def dfs(depart, airports, ticket_count):
+    stack = [[depart, city] for city in airports[depart]]
+    result = []
+    print()
+    print('stack', stack)
+    print()
+    
+    while stack:
+        visited = set()
+        trip = stack.pop()
+        flag = True
+        print()
+        print('first trip', trip)
+        print()
+        visited.add((trip[0], trip[1]))
+        
+        while flag:
+            depart = trip[-1]
+            if airports.get(depart):
+                arrives = airports[depart]
+            else:
+                break
+            print('visit', visited)
+            print('trip', trip)
+            for arrive in arrives:
+                if (depart, arrive) not in visited:
+                    visited.add((depart, arrive))
+                    trip.append(arrive)
+                    break
+            else: 
+                break
 
-def bfs(depart, airports):
-    visited = set()
-    trips = [(depart, city) for city in airports[depart]]
-    print(trips)
-
-    dq = deque([trips])
-    print(dq)
-
-    # while dq:
-    #     trip = dq.popleft()
-    #     depart = trip[1]
-    #     arrives = airports[depart]
-
-    #     for arr in arrives:
-    #         if (depart, arr) in visited:
-    #             continue
-    #         dq.append((depart, arr))
-
-
+            if len(trip) == ticket_count + 1:
+                result.append(trip)         
+    
+    return result
+                       
 
 def solution(tickets):
     answer = []
     cities = set([bill for ticket in tickets for bill in ticket])
+    ticket_count = len(tickets)
     airports = dict()
-    
+    results = []
     for ticket in tickets:
         if airports.get(ticket[0]):
             airports[ticket[0]].append(ticket[1])
         else:
             airports[ticket[0]] = [ticket[1]]
 
-    for city in cities:
-        bfs(city, airports)
-    
-    
-    return answer
+    result = dfs('ICN', airports, ticket_count)
+    results.extend(result)
+    print('result', result)
+
+    print(results)
+    results = [result for result in results if result]
+    results.sort()
+    print(results)
+
+    return results[0]
 
 
 tickets = [['ICN', 'SFO'], ['ICN', 'ATL'], ['SFO', 'ATL'], ['ATL', 'ICN'], ['ATL','SFO']]
+tickets = [['ICN', 'JFK'], ['HND', 'IAD'], ['JFK', 'HND']]
+tickets = [["ICN", 'A'], ['ICN', 'B'], ['B', 'ICN']]
 print(solution(tickets))
