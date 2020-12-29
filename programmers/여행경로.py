939e26b0e4fed7c9bb033498958b6a48f3fb0cce
김진혁 <gandolkim@gmail.com>
2020-12-29 16:01:35 +0900

    여행경로 - 재도전

--- programmers/여행경로.py
+++ programmers/여행경로.py
@@ -1,14 +1,39 @@
+from collections import deque
+
+def bfs(depart, arrive, airports):
+    visited = set()
+
+    visited.add((depart, arrive))
+    trips = [(depart, city) for city in airports[depart]]
+    dq = deque([trips])
+    print(dq)
+
+    # while dq:
+    #     trip = dq.popleft()
+    #     depart = trip[1]
+    #     arrives = airports[depart]
+
+    #     for arr in arrives:
+    #         if (depart, arr) in visited:
+    #             continue
+    #         dq.append((depart, arr))
+
+
+
 def solution(tickets):
     answer = []
+    cities = set([bill for ticket in tickets for bill in ticket])
     airports = dict()
+    
     for ticket in tickets:
         if airports.get(ticket[0]):
             airports[ticket[0]].append(ticket[1])
         else:
             airports[ticket[0]] = [ticket[1]]
 
-    print(airports)
 
+    
+    
     return answer
 
 
