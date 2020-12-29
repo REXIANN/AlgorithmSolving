dcf09ca6a0043fd4d237d0de636de138b0e1a66a
김진혁 <gandolkim@gmail.com>
2020-12-29 18:33:18 +0900

    순위

--- programmers/순위.py
+++ programmers/순위.py
@@ -1,41 +1,43 @@
-def solution(n, results):
-    score = [[0 for __ in range(n)]  for _ in range(n)]
-    winners = [[0 for __ in range(n)]  for _ in range(n)]
-    losers = [[0 for __ in range(n)]  for _ in range(n)]
-    check = [0 for _ in range(n)]
-    INF = 99999
-    for result in results:
-        w, l = result[0] - 1, result[1] - 1
-        score[l][w] = 1
-    
-    for i in range(n):
-        for j in range(n):
-            if i == j:
-                continue
-            
-            if not score[i][j]:
-                winners[i][j] = INF
-            else:
-                winners[i][j] = score[i][j]
-    
-    for i in range(n):
-        check[i] = sum(score[i])
-    
-    
-    for _ in range(n):
-        print(winners[_])
-    print()
-    print(check)
+from collections import deque
 
+def solution(n, results):
+    matrix = [[0] * n for _ in range(n)]
     count = 0
-    # for i in range(n):
-    #     value = sum(winners[i])
-    #     for j in range(n):
-    #         if winners[i][j] != 0:
-    #             value += check[j]        
-    #     if value == n - 1:
-    #         count += 1
+    for result in results:
+        win, lose = result[0] - 1, result[1] - 1
+        matrix[lose][win] = 1
     
+    # for i in range(n):
+    #     print(matrix[i])
+    # print()
+
+    for i in range(n):
+        inner_count = 0
+        # if i is winner
+        visited = set([i])
+        dq = deque([i])
+        while dq:
+            player = dq.popleft()
+            for j in range(n):
+                if matrix[player][j] == 1 and not j in visited:
+                    visited.add(j)
+                    inner_count += 1
+                    dq.append(j)
+        # print(i, 'win', inner_count)
+        
+        # if i is loser
+        visited = set([i])
+        dq = deque([i])
+        while dq:
+            player = dq.popleft()
+            for j in range(n):
+                if matrix[j][player] == 1 and not j in visited:
+                    visited.add(j)
+                    inner_count += 1
+                    dq.append(j)
+        # print(i, 'lose', inner_count)
+        if inner_count >= n - 1:
+            count += 1
     return count
 
 
