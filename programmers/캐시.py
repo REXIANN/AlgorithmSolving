58a1a49f6b57c7d6f6f18a466593d58db168fcc6
김진혁 <gandolkim@gmail.com>
2020-12-30 13:57:41 +0900

    solved

--- programmers/캐시.py
+++ programmers/캐시.py
@@ -1,32 +1,68 @@
-from collections import deque
+import heapq
 
 def solution(cacheSize, cities):
-    heap = [[0, 0, 'a'] for _ in range(cacheSize)] # 사용빈도, 시간, 값
-    is_heap_empty = True
-    
+    h = [] # 사용빈도, 시간, 값
+    count = 0
+
+    print(h)
     for city in cities:
         city = city.lower()
         print(city)
-
-        for h in heap:
-            h[1] += 1
-
+        for i in range(len(h)):
+            h[i][1] -= 1
         
-        for i in range(cacheSize):
+        is_heap_empty = True
+        for i in range(len(h)):
             if h[i][2] == city:
-                h[i][0] += 1
+                h[i][0] -= 1
+                h[i][1] = len(cities)
                 is_heap_empty = False
+                count += 1
                 break
         
         if is_heap_empty:
-            for i in range(cacheSize):
-                min_value = len(cities)
-                if h[i][1] < 
+            count += 5
+            if cacheSize == 0:
+                continue
 
+            if len(h) < cacheSize:
+                h.append([len(cities), len(cities), city])
+                print('result', h)
+                continue
+
+            remove_candidats = []
+            heapq.heapify(h)
+
+            first_h = heapq.heappop(h)
+            remove_candidats.append(first_h)
+            freq = first_h[0]
+            while h:
+                heapq.heapify(h)
+                if h[0][0] == freq:
+                    hh = heapq.heappop(h)
+                    remove_candidats.append(hh)
+                else:
+                    break
+            # print('h', h, 'rm', remove_candidats)
+
+            for i in range(len(remove_candidats)):
+                remove_candidats[i][0], remove_candidats[i][1] = remove_candidats[i][1], remove_candidats[i][0]
+            # print('chrm', remove_candidats)
+            heapq.heapify(remove_candidats)
+            heapq.heappop(remove_candidats)
+            for i in range(len(remove_candidats)):
+                remove_candidats[i][0], remove_candidats[i][1] = remove_candidats[i][1], remove_candidats[i][0]
+            
+            heapq.heappush(h, [len(cities), len(cities), city])
+            # print('changed', remove_candidats)
+            for rm in remove_candidats:
+                h.append(rm)
+        print('result', h)
+        print('count', count)           
     
-    return 1
+    return count
 
-cacheSize = 2
+cacheSize = 5
 
 cities1 = ['Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA', 'Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA']
 cities2 = ['Jeju', 'Pangyo', 'Seoul', 'Jeju', 'Pangyo', 'Seoul', 'Jeju', 'Pangyo', 'Seoul']
@@ -35,4 +71,4 @@ cities4 = ['Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA', 'SanFrancisco', 'Seoul', 
 cities5 = ['Jeju', 'Pangyo', 'NewYork', 'newyork']
 cities6 = ['Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA']
 
-print(solution(cacheSize, cities3))
+print(solution(cacheSize, cities4))
