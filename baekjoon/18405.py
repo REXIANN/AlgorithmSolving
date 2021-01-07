40a6ea5040b902a5efc3bd742009b6f0cd9f2cf0
김진혁 <gandolkim@gmail.com>
2021-01-07 12:24:07 +0900

    경쟁적 전염

--- baekjoon/18405.py
+++ baekjoon/18405.py
@@ -1,28 +1,34 @@
-from collections import deque
 import sys
 sys.stdin = open('18405input.txt', 'r')
+from collections import deque
 
 N, K = map(int, input().split())
 matrix = [list(map(int, input().split())) for _ in range(N)]
 s, x, y = map(int, input().split())
 
-print(N, K, s, x, y)
-print(matrix)
-
-# visited = [[False] * N for _ in range(N)]
 dr = [-1 ,0, 1, 0]
 dc = [0, 1, 0, -1]
+dq = deque()
 
-print(visited)
+count = 0
+for i in range(1, K + 1):
+    for r in range(N):
+        for c in range(N):
+            if matrix[r][c] == i:
+                dq.append((r, c, i))
+                count += 1
 
 for _ in range(s):
-    for i in range(1, k + 1):
-        for r in range(N):
-            for c in range(N):
-                
-                if matrix[r][c] == i:
-                    for i in range(4):
-                        rr = r + dr[i]
-                        cc = c + dc[i]
-                        if 0 <= rr < N and 0 <= cc < N and matrix[rr][cc] == 0:
-                            matrix[rr][cc] = i
+    inner_count = 0
+    for _ in range(count):
+        r, c, i = dq.popleft()
+        for j in range(4):
+            rr = r + dr[j]
+            cc = c + dc[j]
+            if 0 <= rr < N and 0 <= cc < N and matrix[rr][cc] == 0:
+                matrix[rr][cc] = i
+                dq.append((rr, cc, i))
+                inner_count += 1
+    count = inner_count
+
+print(matrix[x - 1][y - 1])
