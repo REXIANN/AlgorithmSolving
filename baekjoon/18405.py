d6a7274474ea0a593d8e699d5b2f6ffc1ca08d73
김진혁 <gandolkim@gmail.com>
2021-01-07 11:07:40 +0900

    경쟁적 전염

--- baekjoon/18405.py
+++ baekjoon/18405.py
@@ -1,2 +1,19 @@
 import sys
 sys.stdin = open('18405input.txt', 'r')
+
+N, K = map(int, input().split())
+matrix = [list(map(int, input().split())) for _ in range(N)]
+s, x, y = map(int, input().split())
+
+print(N, K, s, x, y)
+print(matrix)
+
+visited = [[False] * N for _ in range(N)]
+
+print(visited)
+
+# for _ in range(s):
+#     for i in range(1, k + 1):
+#         for r in range(N):
+#             for c in range(N):
+                # oh no!
