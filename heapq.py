889a495e9fbfaf531e1dedaff20713bb3563d926
김진혁 <gandolkim@gmail.com>
2021-01-25 19:02:01 +0900

    heapq added

--- heapq.py
+++ heapq.py
@@ -1,1 +1,24 @@
-import heapq
+import heapq as hq
+
+h = [1, 2, 5, 17, 49, 25, 37, 19, 99]
+print('Init ')
+print(h)
+print()
+
+# make list h heap!
+hq.heapify(h)
+print('Heapify! ')
+print(h)
+print()
+
+# put a value 48 into h
+hq.heappush(h, 48)
+print('Put 48 ')
+print(h)
+print()
+
+# put another value 1 into h
+hq.heappush(h, 1)
+print('Put 1 ')
+print(h)
+print()
--- priorityqueue.py
+++ priorityqueue.py
@@ -1,1 +1,11 @@
 from queue import PriorityQueue
+
+pq = PriorityQueue(maxsize=2) 
+
+h = [4, 1, 5, 3, 2, 6, 2, 3, 9, 7]
+
+pq.put(4)
+pq.put(8)  
+
+
+print(pq)
