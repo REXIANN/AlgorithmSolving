affedb45c2c1a49ac467a51ceef85bbcdb64018e
김진혁 <gandolkim@gmail.com>
2021-02-19 17:26:44 +0900

    add N으로 표현 - 용준

---
+++ programmers/N으로표현-용준.py
@@ -0,0 +1,21 @@
+  
+def solution(N, number):
+    answer = -1
+    temp = [set() for _ in range(9)]
+    for i in range(1, 9):
+        temp[i].add(int(str(N)*i))
+    for i in range(1, 9):
+        for j in range(i):
+            for num1 in temp[j]:
+                for num2 in temp[i-j]:
+                    temp[i].add(num1+num2)
+                    temp[i].add(num1-num2)
+                    temp[i].add(num1*num2)
+                    if num2 != 0:
+                        temp[i].add(num1//num2)
+        if number in temp[i]:
+            answer = i
+            break
+    return answer
+
+print(solution(3, 27))
